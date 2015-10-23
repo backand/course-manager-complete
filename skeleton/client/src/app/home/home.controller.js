11e6034b53ecb93a97664d13c037e0039ef91e7c
Itay Herskovits <itay@backand.com>
2015-10-23 15:39:23 -0700

    init

---
+++ skeleton/client/src/app/home/home.controller.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular.module('home', [])
+      .config(config)
+      .controller('HomeCtrl',['DataService','date', HomeCtrl]);
+
+  /**
+   * @name  config
+   * @description config block
+   */
+  function config($stateProvider) {
+    $stateProvider
+        .state('root.home', {
+          url: '/home',
+          views: {
+            '@': {
+              templateUrl: 'src/app/home/home.tpl.html',
+              controller: 'HomeCtrl',
+              controllerAs: 'vm',
+              resolve: {
+                data: function(DataService) {
+                  return DataService.get();
+                }
+              }
+            }
+          }
+        });
+  }
+
+  /**
+   * @name  HomeCtrl
+   * @description Controller
+   */
+  function HomeCtrl(DataService, data) {
+    var home = this;
+    home.data = data.data;
+  }
+
+
+})();
